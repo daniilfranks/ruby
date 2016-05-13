be3b22464ca28c7bb618720df172aa43739ed0a1
Danila Babanov <danila_babanov@yahoo.com>
2016-05-13 13:42:54 +0400

    Array

---
+++ array.rb
@@ -0,0 +1,117 @@
+#arr1 = [1,2,3,4,5]
+#puts arr1
+
+#arr2 = Array.new(5){|index| index +1}
+#puts arr2
+
+#arr3 = (1..5).to_a
+#puts arr3
+
+#arr4 = (10..20).to_a
+#puts arr4[-5]
+#puts
+#puts arr4[3..-5]
+
+#arr5 = ["1", 2, "two", 3]
+#puts arr5.size
+#puts arr5
+
+=begin
+arr6 = [1, 4, 30, 233]
+arr7 = ["bmw", "audi", "kia", "lada"]
+puts arr6.max
+puts arr6.min
+puts arr7.max_by{|a| a.size}
+puts arr7.min_by{|a| a.size}
+puts arr6.sort
+puts arr7.sort
+puts arr7.sort_by{|i| i.size}
+puts arr6.uniq
+=end
+
+#arr = [[1,2],[3,4]]
+#puts arr.flatten
+
+#arr = [nil, nil, 1, 'two', "two"]
+#puts arr.compact.uniq
+#arrmax = [[1,2],[3,4]]
+#puts arrmax.map{|i| i.max}
+#puts arrmax.transpose.map{|i| i.max}
+
+#arr = [1,2,3,4,5,6,7,8,9]
+#puts arr.push(10)
+#puts arr.pop
+#puts arr.unshift(0)
+#puts arr.shift
+
+#arr = [1,2,3,4,5,6,7,8,9,"apple"]
+#puts arr.include?(5)
+#puts arr.include?(10)
+#puts arr.include?("apple")
+#puts arr.empty?
+
+#arr = [1,2,3,4,5,6,7,8,9]
+#puts arr.map{|i| i*2}
+
+#arr = [1,2,3,4,5,6,7,8,9]
+#puts arr.find_all{|i| i % 2 == 0}
+#puts arr.find_all{|i| i.odd?}
+#puts arr.find_all{|i| i.even?}
+
+#arr = [1, "tow", 3.0]
+#puts arr
+
+#arr = Array.new(3) 
+#puts arr
+
+=begin
+arr = [1,2,3,4,5,6,7,8,9,10]
+puts arr[0]
+puts arr[-1]
+
+puts arr.first
+puts arr.last
+
+print arr[-5..-1] 
+print arr[0..4]
+puts
+puts arr.take(5)
+puts
+puts arr.drop(5)
+=end
+
+cars = ["bmw", "audi", "kia", "renault", "ford"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ help.rb
@@ -0,0 +1,38 @@
+<h1>Array</h1 >
+arr = []
+arr = Array.new
+arr = ().to_a
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+arr[0]       arr.first
+arr[-1]      arr.last
+
+arr[1..5]    arr.take(5)
+arr[-5..-1]  arr.drop(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
