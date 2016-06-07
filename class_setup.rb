39160bbca1c7640394060a678d6f561e3950ae1f
Danila Babanov <danila_babanov@yahoo.com>
2016-06-07 19:30:32 +0400

    ApiConnector

---
+++ class_setup.rb
@@ -0,0 +1,61 @@
+class ApiConnector
+	attr_accessor :title, :description, :url
+
+	def initialize(title: title(), description: description(), url: url = "google.com")
+		@title = title
+		@description = description
+		@url = url
+	end
+
+	def testing_initializers
+		puts @title
+		puts @description
+		puts @url
+	end
+
+	def api_logger
+		puts "Api connection starting"
+	end
+end
+
+class SmsConnector < ApiConnector
+	def send_sms
+		puts "Sending sms..."
+	end
+end
+
+class PhoneConnector < ApiConnector
+	def api_logger
+		puts "Phone call Api connection starting"
+	end
+
+	def send_phone_call
+		puts "Sending phone call..."
+	end
+end
+
+class MailConnector < ApiConnector
+	def send_email
+		puts "Sending email..."
+	end
+end
+
+api = ApiConnector.new(title: "My title",description: "My description", url: "yahoo.com")
+sms = SmsConnector.new(title: "My title",description: "My description", url: "yahoo.com")
+phone = PhoneConnector.new(title: "My title",description: "My description", url: "yahoo.com")
+email = MailConnector.new(title: "My title",description: "My description", url: "yahoo.com")
+
+sms.send_sms
+phone.send_phone_call
+email.send_email
+api.api_logger
+phone.api_logger
+
+
+
+
+
+
+
+
+
---
+++ edutechional.rb
@@ -0,0 +1,132 @@
+p name = "Den"
+p address = ["Kazan", "Mira 12"]
+
+=begin
+p "What is you name?"
+name = gets.chomp
+
+if name == "Den"
+	p true
+else
+	p false
+end
+=end
+
+p "text".upcase
+p "Text".downcase
+p "text".reverse
+
+str = "  Hi world spam   "
+p str.gsub "spam",""
+p str.strip
+p str.size
+p str.split.size
+
+=begin
+class Test
+	def self.one
+		p "one"
+	end
+
+	def two
+		p "two"
+	end
+end
+
+a = Test.new
+Test.one
+a.two
+=end
+
+i = 0
+while i < 10
+	i += 1
+	puts "Hi Ruby"
+end
+
+arr = [12,14,34,67,89]
+arr.each do |i|
+	p i
+end
+
+for i in 0..10
+	p i
+end
+
+(1..10).to_a.select {|x| x.even?}
+(1..10).to_a.select do |x|
+	x.even?
+end
+
+arr = %w(The quick brown fox over the lazy dog)
+arr.select {|x| x.length > 5}
+
+["1","2.0","77"].map{|x| p x.to_i}
+("a".."g").to_a.map{|i| p i*2}
+total = 0
+[12,45,23,74].each do |i|
+	 total += i
+end
+p total
+
+p [12,45,23,74].inject(&:+)
+p [12,45,23,74].inject(&:*)
+
+arr = ["one","two","true"]
+p arr.join(",")
+
+people ={den: 322, oleg: 12, climent: 94}
+p people[:den]
+people.delete(:oleg)
+p people
+
+people.each_key do |key|
+	puts key
+end
+
+=begin
+class ApiConnector
+	attr_accessor :title, :description, :url
+
+	def test_method
+		puts "testing class call"
+	end
+end
+
+api = ApiConnector.new
+p api.url = "google.com"
+api.test_method
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
