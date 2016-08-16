d43610aac4cc311479a6b11e1ef7cba7d6513cd2
Danila Babanov <danila_babanov@yahoo.com>
2016-08-16 11:15:59 +0400

    basics

--- 8ball/README.md
+++
@@ -1,6 +0,0 @@
-#Magic 8-Ball. a Ruby program that gives you an answer to any question you might have.
-
-More info:
-https://en.wikipedia.org/wiki/Magic_8-Ball
-
-Command to run: 'ruby 8ball.rb'
---
+++ IntroductiontoProgramming/basics.rb
@@ -0,0 +1,36 @@
+a = 'den'
+p a
+p "Text #{a}"
+
+#integers
+p 1,20,100,2000
+#floats
+p 1.12, 4.435345, 123.212434
+
+#operations
+p 1+2
+p 4-3
+p 2*2
+p 4/2
+p 20/5
+p 23%5
+p 10.0/2
+p 27/7.2
+
+p 2==2
+p 3==5
+
+p 'fil'=='fil'
+p 'fil'=='yound'
+p '1'==1
+p 'Hi'+' world'
+p '1'+'1'
+
+p '7'.to_i
+p '7'.to_f
+p 7.to_s
+
+arr = [1,2,3]
+p arr[1]
+hash = {:car => "kia"}
+p hash[:car]
