638586adb9edc203d995982566713e99c3f6fcf6
Danila Babanov <danila_babanov@yahoo.com>
2016-08-28 14:54:08 +0400

    hash book phone

--- IntroductiontoProgramming/app37.rb
+++ IntroductiontoProgramming/app37.rb
@@ -8,3 +8,31 @@ b = a-b
 a = a-b
 #==========================================================
 
+arr = ["den","jok","anna","frodo"]
+arr.each_with_index do |name,i|
+	puts "#{i+1}. #{name} "
+end
+
+options = {:fotn_size => 10, :font_family => "Arial"}
+options[:name] = "Den"
+puts options
+puts options[:font_family]
+
+#===========================================================
+hash = {}
+
+loop do
+	puts "Add name"
+	name = gets.strip.capitalize
+
+	if name == ""
+		break
+	end
+
+	puts "Add phone number"
+	phone = gets.strip.to_i
+
+	hash[name] = phone
+
+end
+puts hash
