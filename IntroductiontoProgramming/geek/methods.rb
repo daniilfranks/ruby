978d08b76b6ec8142fcc1112c4a8adb48a7f25f1
Danila Babanov <danila_babanov@yahoo.com>
2016-09-14 15:38:45 +0400

    methods

--- IntroductiontoProgramming/geek/methods.rb
+++ IntroductiontoProgramming/geek/methods.rb
@@ -21,3 +21,74 @@ p odd 1
 p odd 2
 p even 1
 p even 2
+
+
+def hi
+	yield
+end
+
+hi { p "Hi Ruby" }
+
+hi_name = lambda {|name| puts "Hi, #{name}"}
+hi_name.call "Den"
+
+1.upto(3){puts "Upto"}
+3.downto(1){puts "Downto"}
+
+
+def times_tables
+  1.upto(10) { |i| 1.upto(10) { |j| print i.to_s + " x " + j.to_s + " = ", j * i, "\n"} }
+end
+
+times_tables
+
+
+hi_proc = proc {"Hi proc"}
+puts hi_proc.call
+
+name_proc = proc {|name| puts "Hi, #{name}"}
+name_proc.call "Den"
+
+
+def namer hi, name 
+	return hi + name
+end
+
+p namer "Hi", " Den"
+
+
+def numer val 
+	return val + 1
+end
+
+p numer 5
+
+
+
+def say_gn name
+	return "Good night, #{name}"
+end
+
+puts say_gn "Ma"
+
+
+def hi_yield
+	yield
+	yield
+	yield
+end
+
+hi_yield {p "Hi yield"}
+
+
+def den
+	if block_given?
+		yield
+		yield
+	else
+		puts "I m blockless!"
+	end
+end
+
+den
+den {p "Hi den"}
