9f50e705c3b095fb227d63589631a0a383499459
Danila Babanov <danila_babanov@yahoo.com>
2016-09-19 21:32:26 +0400

    animal

--- IntroductiontoProgramming/book/headfirst/animals.rb
+++ IntroductiontoProgramming/book/headfirst/animals.rb
@@ -1,14 +1,4 @@
-class Bird   
-	def talk     
-		puts "Chirp! Chirp!"   
-	end  
-
-	def move(destination)     
-		puts "Flying to the #{destination}."   
-	end 
-end 
-
-class Dog   
+class Animal
 	attr_accessor :name, :age
 
 	def report_age
@@ -22,9 +12,22 @@ class Dog
 	def move(destination)     
 		"#{@name} runs to the #{destination}."   
 	end 
+end
+
+class Bird < Animal
+	def talk     
+		puts "Chirp! Chirp!"   
+	end  
+
+	def move(destination)     
+		puts "Flying to the #{destination}."   
+	end 
 end 
 
-class Cat   
+class Dog < Animal 
+end 
+
+class Cat < Animal 
 	def talk     
  		puts "Meow!"   
  	end  
@@ -39,7 +42,9 @@ dog = Dog.new
 cat = Cat.new 
 bird.move("tree") 
 bird.talk 
+cat.name = "snowbal"
 cat.move("house")
+cat.talk
 
 dog.name = "Rex"
 dog.age = 3
