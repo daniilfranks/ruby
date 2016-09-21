48013eafa2d9726d2fbdc132868a94136c6a9bc5
Danila Babanov <danila_babanov@yahoo.com>
2016-09-21 21:50:10 +0400

    file array

--- IntroductiontoProgramming/book/headfirst/array.rb
+++ IntroductiontoProgramming/book/headfirst/array.rb
@@ -24,3 +24,73 @@ while index < array.length
 	puts array[index]
 	index += 1
 end
+
+p [1,2,3,4,5].find_all { |n| n.even? }
+p [1,2,3,4,5].find_all { |n| n.odd? }
+
+
+public
+	def new_find_all
+		matching_items = []
+		self.each do |item|
+			if yield(item)
+				matching_items << item
+			end
+		end
+		matching_items
+	end
+
+	def new_reject
+		matching_items = []
+		self.each do |item|
+			unless yield(item)
+				matching_items << item
+			end
+		end
+		matching_items
+	end
+
+p [1,2,3,4,5].new_find_all { |n| n.even? }
+p [1,2,3,4,5].new_find_all { |n| n.odd? }
+
+p [1,2,3,4,5].new_reject { |n| n.even? }
+p [1,2,3,4,5].new_reject { |n| n.odd? }
+
+
+
+numbers = [2,3,4]
+squ = []
+
+numbers.each do |n|
+	squ << n ** 2
+end
+p squ
+
+
+
+phone_numbers = ["1-800-555-0199", "1-402-555-0123"] 
+area_codes = [] 
+
+phone_numbers.each do |phone_number|   
+	area_codes << phone_number.split("-")[1] 
+end 
+p area_codes 
+
+
+numbers = [2,3,4].map { |number| number ** 2 } 
+phone_numbers = ["1-800-555-0199", "1-402-555-0123"].map do |phone|   
+	phone.split("-")[1]
+end
+
+
+public
+	def new_map   
+		results = []   
+		self.each do |item|     
+			results << yield(item)   
+		end   
+		results 
+	end 
+
+arr = [1,23,324,234,12,35]
+arr.new_map { |i| p i * 2 }
--- IntroductiontoProgramming/book/headfirst/file.rb
+++ IntroductiontoProgramming/book/headfirst/file.rb
@@ -33,3 +33,39 @@ puts relevant_lines
 
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 puts relevant_lines
+
+
+p a = "1-800-122-0002".split("-")
+p b = "his/her".split("/")
+p c ="apple, avocado, anvil".split(", ")
+p a.find_index("800")
+p b.find_index("his")
+p c.find_index("anvil")
+
+
+string = lines.first
+words = string.split(" ")
+p words
+
+
+def find_adjective(string)   
+	words = string.split(" ")   
+	index = words.find_index("is")   
+	words[index + 1] 
+end 
+
+lines = [] 
+File.open("reviews.txt") do |review_file|   
+ 	lines = review_file.readlines 
+end 
+
+relevant_lines = lines.find_all { |line| line.include?("Truncated") } 
+reviews = relevant_lines.reject { |line| line.include?("--") } 
+
+adjectives = reviews.map do |review|   
+	adjective = find_adjective(review)   
+	"'#{adjective.capitalize}'" 
+end 
+
+puts "The critics agree, Truncated is:" 
+puts adjectives 
