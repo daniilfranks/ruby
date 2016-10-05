d012073dfe0641212886e55edc43d9524555dfa4
Danila Babanov <danila_babanov@yahoo.com>
2016-10-05 21:56:49 +0400

    oop

--- IntroductiontoProgramming/book/app11.rb
+++ IntroductiontoProgramming/book/app11.rb
@@ -29,3 +29,71 @@ end
 u = User.new
 u.say_secret_with_self # => "secret"
 
+
+arr = (1..10).to_a
+b = arr.select { |i| i.even? }
+p b
+
+
+
+def per_squ(side)
+	side * 4
+end
+
+p per_squ(5)
+
+
+def are_squ(side)
+	side * side
+end
+
+p are_squ(5)
+
+def per_tri(side1, side2, side3)
+	side1 + side2 + side3
+end
+
+p per_tri(3,4,10)
+
+
+class Square
+	def initialize(length)
+		@length = length
+	end
+
+	def area
+		@length * @length
+	end
+
+	def perimeter
+		@length * 4
+	end
+end
+
+square = Square.new(18)
+p square.area
+p square.perimeter
+
+
+class Triangle
+	def initialize(base_width, height, side1, side2, side3)
+		@base_width = base_width
+		@height     = height
+		@side1      = side1
+		@side2      = side2
+		@side3      = side3
+	end
+
+	def area
+		@base_width * @height
+	end
+
+	def perimeter
+		@side1 + @side2 + @side3
+	end
+end
+
+tri = Triangle.new(10, 20 , 10.9, 17.4, 4.93)
+p tri.area
+p tri.perimeter
+
