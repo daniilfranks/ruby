489b8ca9a72cf3b16f8dbad56f4b70db392823be
Danila Babanov <danila_babanov@yahoo.com>
2016-10-09 13:21:59 +0400

    oop

--- IntroductiontoProgramming/book/app16.rb
+++ IntroductiontoProgramming/book/app16.rb
@@ -1,15 +1,22 @@
 class Gear
-	attr_reader :chairing, :cog
+	attr_reader :chairing, :cog, :rim, :tire
 
-	def initialize(chairing, cog)
+	def initialize(chairing, cog, rim, tire)
 		@chairing = chairing
 		@cog      = cog
+		@rim      = rim
+		@tire     = tire
 	end
 
 	def ratio
 		chairing / cog.to_f
 	end
+
+	def gear_inches
+		ratio * (rim + (tire * 2))
+	end
 end
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 17).ratio
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, 24, 1.25).gear_inches
+
