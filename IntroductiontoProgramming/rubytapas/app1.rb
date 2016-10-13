8b816ab2ac861bd5244644c950886b740e8e7b84
Danila Babanov <danila_babanov@yahoo.com>
2016-10-13 11:15:42 +0400

    super in modules

--- IntroductiontoProgramming/rubytapas/app1.rb
+++ IntroductiontoProgramming/rubytapas/app1.rb
@@ -30,3 +30,31 @@ port = ENV.fetch('PORT'){8080}.to_i
 p port
 
 
+module YeOlde
+	def hello(subject="World")
+		if defined?(super)
+			puts super
+		else
+			puts "Good morrow, #{subject}!"
+		end 
+		puts "Well met indeed!"
+	end
+end
+
+class Greeter
+	def hello(subject)
+		puts "Hello, #{subject}"
+	end
+end
+
+class GreeterChild < Greeter
+	include YeOlde
+end
+
+class NonGreeter
+	include YeOlde
+end
+
+
+GreeterChild.new.hello("Bob")
+NonGreeter.new.hello("Sally")
