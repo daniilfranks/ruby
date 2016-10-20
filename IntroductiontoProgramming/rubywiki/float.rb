46637f5299d47290bcdda5ac20c0ed0de447d963
Danila Babanov <danila_babanov@yahoo.com>
2016-10-20 13:48:38 +0300

    float

---
+++ IntroductiontoProgramming/rubywiki/float.rb
@@ -0,0 +1,35 @@
+p 100.50 % 12
+p 100 % 3.3
+
+p 2.0 ** 3
+p 1.0 == 1
+
+p -99.abs
+p -7.abs
+p -3.5.abs
+
+p 1.0.ceil
+p 1.1.ceil
+p 1.2.ceil
+p 1.5.ceil
+p 1.7.ceil
+
+p 1.0.floor
+p 1.1.floor
+p 1.2.floor
+p 1.5.floor
+p 1.7.floor
+
+p "="*20
+
+p 1.4.round
+p 1.5.round
+p 1.6.round
+
+p 1.23456789.round(2)
+p 1.23456789.round(3)
+p 1.23456789.round(4)
+p 1.23456789.round(5)
+
+p 1.5.to_i
+p 2.to_f
