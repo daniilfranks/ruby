242c9fdc931cb012bd5a9f45384e2ab9d239c61e
Danila Babanov <danila_babanov@yahoo.com>
2016-11-09 20:19:34 +0300

    regex

---
+++ IntroductiontoProgramming/rubytapas/99.rb
@@ -0,0 +1,5 @@
+p '085 Ignore Arguments'.match(/^\d{3}/)
+p '085 Ignore Arguments'.match(/^\d{3}/)[0]
+p '085 Ignore Arguments'[/^\d{3}/]
+p '085 Ignore Arguments'[/^\d{3} (.+)/]
+p '085 Ignore Arguments'[/^\d{3} (.+)/, 1]
