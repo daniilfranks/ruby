e6d1048576d0c6d3d9a060c44b470adcbb7ca2b9
Danila Babanov <danila_babanov@yahoo.com>
2016-11-27 12:11:55 +0300

    json

--- intensive/json2.rb
+++ intensive/json2.rb
@@ -3,16 +3,15 @@ require 'json'
 require 'ostruct'
 
 begin
-  p parse_time = File.ctime("parseuser.json")
-  p time = Time.new
-  unless 50 >= (time - parse_time).to_i
+  parse_time = File.ctime("parseuser.json")
+  time = Time.new
+  unless 500 > (time - parse_time).to_i
     uri = open('https://api.github.com/users/danilababanov')
     user = JSON.parse(uri.read)
+    File.open('parseuser.json', 'w') do |w|
+      w << user
+    end
   end
 rescue StandardError => e
   puts e
-end
-
-File.open('parseuser.json', 'w') do |w|
-  w << user
 end
