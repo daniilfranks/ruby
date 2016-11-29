a092c6271a3521424fadaa7d3435180c6406be1f
Danila Babanov <danila_babanov@yahoo.com>
2016-11-29 09:33:33 +0300

    yaml

--- intensive/yaml.rb
+++ intensive/yaml.rb
@@ -7,3 +7,9 @@ p YAML.dump('foo')
 p [1,2,3,4,5].to_yaml
 p hash = { name: 'Den', age: 25 }.to_yaml
 
+data = { 'fruits': ['orange', 'apple', 'grape'] }
+data = YAML.dump(data, File.open('sample.yml', 'w'))
+
+
+data = open('sample2.yml', 'r') { |f| YAML.load(f) }
+p data
