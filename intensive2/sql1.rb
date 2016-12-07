3b24e05a7a16f431f19a7314b8752345f4bce227
Danila Babanov <danila_babanov@yahoo.com>
2016-12-07 19:12:08 +0300

    sqlite

--- intensive2/sql1.rb
+++ intensive2/sql1.rb
@@ -3,11 +3,100 @@ require 'sqlite3'
 begin
   db = SQLite3::Database.open('test.db')
 
+  id = 1
+
+  inquiry = db.prepare('SELECT * FROM cars WHERE Id=?')
+  inquiry.bind_param 1, id
+
+  rs = inquiry.execute
+  string = rs.next
+
+  puts string.join("\s")
+
+  inquiry.close
+  db.close
+rescue SQLite3::Exception => e
+  p 'Exception occurred'
+  p e
+end
+
+p '*'*30
+
+begin
+  db = SQLite3::Database.open('test.db')
+
+  string = db.get_first_row('SELECT * FROM cars WHERE Id=?', 1)
+  puts string.join("\s")
+
+  db.close
+rescue SQLite3::Exception => e
+  p 'Exception occurred'
+  p e
+end
+
+p '*'*30
+
+begin
+  db = SQLite3::Database.open('test.db')
+
+  value = db.get_first_value("SELECT Price FROM cars WHERE Name=?", 'Audi')
+  puts value
+
+  db.close
+rescue SQLite3::Exception => e
+  p 'Exception occurred'
+  p e
+end
+
+p '*'*30
+
+begin
+  db = SQLite3::Database.open('test.db')
+
   inquiry = db.prepare('SELECT * FROM cars LIMIT 5')
   array = inquiry.execute
 
   array.each { |i| puts i.join("\s") }
+
+  inquiry.close
+  db.close
+rescue SQLite3::Exception => e
+  p 'Exception occurred'
+  p e
+end
+
+p '*'*30
+
+begin
+  db = SQLite3::Database.open('test.db')
+
+  inquiry = db.prepare('SELECT * FROM cars LIMIT 5')
+  array = inquiry.execute
+
+  while (row = array.next) do 
+    puts row.join("\s")
+  end
+
+  inquiry.close
+  db.close
+rescue SQLite3::Exception => e
+  p 'Exception occurred'
+  p e
+end
+
+p '*'*30
+
+begin
+  db = SQLite3::Database.open('test.db')
+  db.results_as_hash = true
+
+  array = db.execute('SELECT * FROM cars LIMIT 5')
   
+  array.each do |i|
+    printf "%s %s %s\n", i['Id'], i['name'], i['price']
+  end
+
+  db.close
 rescue SQLite3::Exception => e
   p 'Exception occurred'
   p e
