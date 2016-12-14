50ff0a6d997aa0ce5f4c1778272b2fb2f52a415f
Danila Babanov <danila_babanov@yahoo.com>
2016-12-14 14:28:12 +0300

    sql

--- intensive2/sql15.rb
+++ intensive2/sql15.rb
@@ -18,8 +18,7 @@ end
 begin
   db = SQLite3::Database.new('test.db')
   db.execute('CREATE TABLE company(
-              Id INTEGER PRIMARY KEY,
-              com_id             text(4)  NOT NULL UNIQUE,
+              com_id             text(4)  NOT NULL UNIQUE PRIMARY KEY,
               com_name           text(15) NOT NULL UNIQUE
             )')
 
@@ -32,8 +31,7 @@ end
 begin
   db = SQLite3::Database.new('test.db')
   db.execute('CREATE TABLE orders(
-              Id INTEGER PRIMARY KEY,
-              ord_id             text(4)  NOT NULL,
+              ord_id             text(4)  NOT NULL PRIMARY KEY,
               item_id            text(4)  NOT NULL UNIQUE,
               ord_date           date     NOT NULL UNIQUE,
               ord_qty            integer  NOT NULL,
@@ -49,10 +47,11 @@ end
 begin
   db = SQLite3::Database.new('test.db')
   db.execute('CREATE TABLE item(
-              Id INTEGER PRIMARY KEY,
-              item_id             text(4)  NOT NULL UNIQUE,
+              item_id             text(4)  NOT NULL UNIQUE PRIMARY KEY,
               item_desc           text(20) NOT NULL,
-              rate                integer  CHECK (rate > 0 AND rate < 100)
+              rate                integer  CHECK (rate > 0 AND rate < 100),
+              icom_id              text(4)  NOT NULL,
+              FOREIGN KEY(icom_id) REFERENCES company(com_id)
             )')
 
   db.close
@@ -68,6 +67,12 @@ begin
               VALUES(?, ?)', 'Com1', 'DEN BOB LTD.')
   db.execute('INSERT INTO company(com_id, com_name) 
               VALUES(?, ?)', 'Com2', 'DELTA UA LTD.')
+  db.execute('INSERT INTO company(com_id, com_name) 
+              VALUES(?, ?)', 'Com3', 'ASD INC.')
+  db.execute('INSERT INTO company(com_id, com_name) 
+              VALUES(?, ?)', 'Com4', 'TSNRU CORP.')
+  db.execute('INSERT INTO company(com_id, com_name) 
+              VALUES(?, ?)', 'Com5', 'EDIT CORP.')
   db.execute('INSERT INTO company(com_id) 
               VALUES(?)', 'Com2')
   db.execute('INSERT INTO company(com_id) 
@@ -96,14 +101,28 @@ end
 
 begin
   db = SQLite3::Database.new('test.db')
-  db.execute('INSERT INTO item(item_id, item_desc, rate) 
-              VALUES(?, ?, ?)', 'ITM1', 'Pean-n-Nut', 15)
-  db.execute('INSERT INTO item(item_id, item_desc, rate) 
-              VALUES(?, ?, ?)', 'ITM2', 'Cool Pesta', 10)
-  db.execute('INSERT INTO item(item_id, item_desc, rate) 
-              VALUES(?, ?, ?)', 'ITM3', 'Cool Pesta', 110)
-  db.execute('INSERT INTO item(item_id, item_desc, rate) 
-              VALUES(?, ?, ?)', 'ITM4', 'Cool Pesta', 0)
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM1', 'Pean-n-Nut', 15, 'Com1')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM2', 'Cool Pesta', 10, 'Com2')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM3', 'Pena calada', 30, 'Com2')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM4', 'B 52', 48, 'Com3')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM5', 'ZZZ YA', 51, 'Com4')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM6', 'Mini Car', 25, 'Com2')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM7', 'Delete Item', 20, 'Com1')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM8', 'Edit item', 25, 'Com1')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM3', 'Cool Pesta', 110, 'Com1')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM4', 'Cool Pesta', 0, 'Com1')
+  db.execute('INSERT INTO item(item_id, item_desc, rate, icom_id) 
+              VALUES(?, ?, ?, ?)', 'ITM15', 'Tester', 0, 'Com10')
 
   db.close
 rescue SQLite3::Exception => e
@@ -148,3 +167,37 @@ rescue SQLite3::Exception => e
   puts 'Exception occurred'
   puts e
 end
+
+#Update
+begin
+  db = SQLite3::Database.new('test.db')
+  db.execute('UPDATE company SET com_id = ? WHERE com_id = ?', 'Com6', 'Com5')
+
+  db.close
+rescue SQLite3::Exception => e
+  puts 'Exception occurred'
+  puts e
+end
+
+#Delete
+begin
+  db = SQLite3::Database.new('test.db')
+  db.execute('DELETE FROM company WHERE com_id = ?', 'Com4')
+
+  db.close
+rescue SQLite3::Exception => e
+  puts 'Exception occurred'
+  puts e
+end
+
+begin
+  db = SQLite3::Database.new('test.db')
+  array = db.execute('SELECT * FROM company')
+  pp array
+
+  db.close
+rescue SQLite3::Exception => e
+  puts 'Exception occurred'
+  puts e
+end
+
