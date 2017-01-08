01f013dca24fc97f1dfa5d89e4964dc91b9d68e6
Danila Babanov <danila_babanov@yahoo.com>
2017-01-08 13:47:05 +0300

    test1

--- minitest/test1/lib/user.rb
+++ minitest/test1/lib/user.rb
@@ -1,16 +1,30 @@
 # Default class User
 class User
-  attr_accessor :name, :age, :city
+  attr_accessor :name, :age, :city, :items
 
   def initialize(name, age, city)
     @name = name
     @age  = age
     @city = city
+    @items  = []
   end
 
   def hello
     "Hello #{@name}, #{@age}, #{@city}"
   end
+
+  def add_items(*item)
+  	@items << item
+  end
+
+  def show_items
+  	@items.join(', ')
+  end
 end
 
 # ruby -I lib test/test_user.rb
+user = User.new('Den', 25, 'Kazan')
+
+user.add_items('cat', 'dog')
+p user.items
+p user.show_items
--- minitest/test1/test/test_user.rb
+++ minitest/test1/test/test_user.rb
@@ -2,16 +2,28 @@ require 'minitest/autorun'
 require 'user'
 
 class TestUser < Minitest::Test
+	# Метод запускается перед каждым тестом
 	def setup
 		@user = User.new('Den', 25, 'Kazan')
+		@user.add_items('cat', 'dog')
+	end
+
+	# Метод вызывается после каждого теста
+	def teardown
+		# Пример закрыть дб
+		# db.close
 	end
 
 	def test_user_name
 		assert(@user.name == 'Den')
+
+		# Проверка на экземпляр класса
+		assert_instance_of(String, 'Den')
 	end
 
 	def test_user_age
 		assert(@user.age == 25)
+		assert_instance_of(Fixnum, 25)
 	end
 
 	def test_user_city
@@ -19,6 +31,26 @@ class TestUser < Minitest::Test
 	end
 
 	def test_hello
-		assert(@user.hello == "Hello Den, 25, Kazan")
+		assert(@user.hello == 'Hello Den, 25, Kazan')
+	end
+
+	def test_add_items
+		assert(@user.items == [['cat', 'dog']])
+	end
+
+	# Простое сравнение 2 условий
+	# def test_show_items
+	# 	assert(@user.show_items == 'cat, dog')
+	# end
+
+	# Простое сравнение 2 условий
+	def test_show_items
+		assert_equal(@user.show_items, 'cat, dog')
+	end
+
+	# Поиск объекта в масиве
+	def test_items_includes
+		@user.add_items('car')
+		assert_includes(@user.items, ['car'])
 	end
 end
