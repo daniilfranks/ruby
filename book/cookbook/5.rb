dde078cd7f83bd09c5515ef0ccce1b96a6c02647
Danila Babanov <danila_babanov@yahoo.com>
2017-01-26 19:15:20 +0300

    class

---
+++ book/cookbook/5.rb
@@ -0,0 +1,68 @@
+class Translate
+	@@translation = { :en => 'Hi world', :rus => 'Привет мир'}
+
+	def initialize(language = :en)
+		@language = language
+		#@translation = { :en => 'Hi world', :rus => 'Привет мир'}
+	end
+
+	def translate
+		#@@translation[@language]
+		@@translation[@language]
+	end
+end
+
+p default = Translate.new
+p default.translate
+p Translate.new(:rus).translate
+
+class Module
+	def class_attr_reader(*symbols)
+		symbols.each do |symbol|
+			self.class.send(:define_method, symbol) do
+				class_variable_get("@@#{symbol}")
+			end
+		end
+	end
+
+	def class_attr_writer(*symbols)
+		symbols.each do |symbol|
+			self.class.send(:define_method, "#{symbol}=") do
+				class_variable_set("@@#{symbol}", value)
+			end
+		end
+	end
+
+	def class_attr_accessor(*symbols)
+		class_attr_reader(*symbols)
+		class_attr_writer(*symbols)
+	end
+end
+
+
+class Fate
+	NAMES = ['Leonard', 'Howard', 'Glen'].freeze
+	@@number_instantiated = 0
+
+	class_attr_reader :number_instantiated
+
+	def initialize
+		if @@number_instantiated >= NAMES.size
+			raise ArgumentError, 'Three Fate!'
+		end
+		@name = NAMES[@@number_instantiated]
+		@@number_instantiated += 1
+		puts "I give you #{@name}"
+	end
+end
+
+p Fate.number_instantiated
+p Fate.new
+p Fate.number_instantiated
+p Fate.new
+p Fate.number_instantiated
+p Fate.new
+p Fate.number_instantiated
+
+p '*'*40
+
