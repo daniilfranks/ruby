0364009b0951cc4bd289dbc25e40236ee0c2bb00
Danila Babanov <danila_babanov@yahoo.com>
2017-01-27 17:30:58 +0300

    module

--- book/cookbook/7.rb
+++ book/cookbook/7.rb
@@ -22,11 +22,57 @@ module SuperPower
 end
 
 class SuperAss
-	include SuperPower
+	extend SuperPower
 end
 
 super_ass = SuperAss.new
 #p super_ass.fly
 #p super_ass.leap(10)
 #p super_ass.class_method
+
+module TimeJs
+	attr_reader :time_create
+
+	def initialize
+		@time_create = Time.now
+	end
+
+	def show_time
+		Time.now - @time_create
+	end
+end
+
+class Character
+	include TimeJs
+
+	attr_reader :name
+
+	def initialize(name)
+		@name = name
+		super()
+	end
+end
+
+c = Character.new('Den')
+p c
+p c.time_create
+p c.show_time
+
+p '*'*40
+
+p 1.class
+p 1.class.name
+p 1.class.superclass
+
+#p Object.methods
+#p String.methods
+
+
+
+
+
+
+
+
+
 
