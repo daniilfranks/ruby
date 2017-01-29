5da12cf0ec87b721ad4d753850753ae59ecbf67b
Danila Babanov <danila_babanov@yahoo.com>
2017-01-29 13:10:05 +0300

    object

--- book/confident_ruby/1.rb
+++ book/confident_ruby/1.rb
@@ -8,7 +8,7 @@ p 1.class
 p self.class
 p self.class.superclass
 
-
+# Clone
 class User
 	attr_accessor :name, :age
 
@@ -37,6 +37,71 @@ p ramon = carl
 p ramon.object_id
 p ramon.to_s
 
+p '*'*45
 
+# define_singleton_method
+class A
+	class << self
+		def class_name
+			to_s
+		end
+	end
 
+	def self.name
+		to_s
+	end
+end
 
+A.define_singleton_method(:who_am_i) do 
+	"I am #{class_name}"
+end
+
+p A.class_name
+p A.who_am_i
+
+A.define_singleton_method(:hello_class) do 
+	"Hi my name class #{to_s}"
+end
+
+p A.hello_class
+
+p '*'*45
+
+1.display
+puts
+'Hello display'.display
+puts
+[1,2,3].display
+puts
+
+# dup
+class Foo
+	attr_accessor :foo
+end
+
+module Bar
+	def bar
+		'bar'
+	end
+end
+
+p foo = Foo.new
+p foo.object_id
+p foo.foo = 'hi class foo'
+foo.extend(Bar)
+p foo.bar
+
+p '*'*45
+
+p f1 = foo.clone
+p f1.object_id
+p f1.foo
+p f1.bar
+
+p '*'*45
+
+p f2 = foo.dup
+p f2.object_id
+p f2.foo
+# Error method bar
+# p f2.bar
