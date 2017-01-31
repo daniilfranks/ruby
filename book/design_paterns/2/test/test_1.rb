5f091681abbae1087698e4a0b072ae88b223dd96
Danila Babanov <danila_babanov@yahoo.com>
2017-01-31 21:10:49 +0300

    test

---
+++ book/design_paterns/2/test/test_1.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require_relative '../1'
+
+class TestCar < Minitest::Test
+	def setup
+		@bank = BankAccount.new('account001')
+		@bank.instance_variable_set(:@balance, 500)
+	end
+
+	def test_account_owner
+		assert_equal('account001', @bank.account_owner)
+	end
+
+	def test_balance
+		assert_equal(500, @bank.instance_variable_get(:@balance))
+	end
+
+	def test_deposite
+		assert_equal(700, @bank.deposite(200))
+	end
+
+	def test_withdraw
+		assert_equal(300, @bank.withdraw(200))
+	end
+
+	def test_set_balance
+		assert_equal(2000 , @bank.set_balance(2000))
+	end
+end
