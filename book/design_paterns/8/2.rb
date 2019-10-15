d1c5b66fb63ee9a0f92d46e860da425e894c0558
Danila Babanov <danila_babanov@yahoo.com>
2019-10-15 10:51:01 +0300

    app

--- book/design_paterns/8/2.rb
+++
@@ -1,3 +0,0 @@
-array = []
-' Hello word! I love ruby. '.split('').each { |s| array << s.ord }
-p array
---
+++ kr.rb
@@ -0,0 +1,7 @@
+price = 2300000
+year = 240
+pro = 6
+
+puts ((2300000* 0.06) * 20 + 2300000) / 240
+
+<%= (((@novostroyki.price1 * 0.06) * 20 + @novostroyki.price1) / 240).round %>
